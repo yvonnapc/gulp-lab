eb69519fe0ae38f09b169f85925775fd0f6d6713
Yvonna Contreras <yvonnap.contreras@gmail.com>
2017-04-04 13:40:55 -0700

    Merge coffee task using eventstream

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var coffee = require('gulp-coffee');
+var es = require('event-stream');
 
-gulp.task('coffee', function(){
-  return gulp.src('src/*.coffee')
-  .pipe(coffee())
-  .pipe(gulp.dest('src'));
-});
+gulp.task('scripts', function(){
+  var javaScriptFromCoffeeScript = gulp.src('src/*.coffee')
+    .pipe(coffee());
 
-gulp.task('scripts', ['coffee'], function(){
-  return gulp.src('src/*.js')
+  var js = gulp.src('src/*js');
+
+  return es.merge(javaScriptFromCoffeeScript, js)
   .pipe(concat('all.min.js'))
   .pipe(uglify())
   .pipe(gulp.dest('dist'));
